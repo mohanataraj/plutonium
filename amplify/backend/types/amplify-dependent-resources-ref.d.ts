66564f67ed390da5750332077d12a3cd4b1b6a18
mohanataraj <mohan1519@gmail.com>
2024-06-13 22:03:23 -0700

    removed amplify

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++
@@ -1,9 +0,0 @@
-export type AmplifyDependentResourcesAttributes = {
-  "api": {
-    "livelandingpage": {
-      "GraphQLAPIEndpointOutput": "string",
-      "GraphQLAPIIdOutput": "string",
-      "GraphQLAPIKeyOutput": "string"
-    }
-  }
-}
